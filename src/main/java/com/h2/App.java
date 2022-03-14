ccd8f0bd896723dd8b29c1a78bc8365124b0e0ec
swathikchgmail <32529219+swathikchgmail@users.noreply.github.com>
2022-03-14 12:11:44 -0700

    adding new class and fix a method

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,6 +12,13 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return 2 * number;
+    }
+    public static final int add(int[] numbers) {
+        int sum = 0;
+        for(int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
     }
 }
