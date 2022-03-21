cb31d490b7a999bac9d043b4ef72ebab3894734e
swathi.chadalavada <swathi.chadalavada@servicenow.com>
2022-03-20 17:13:23 -0700

    new changes

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -4,24 +4,30 @@ import java.time.LocalDate;
 import java.time.YearMonth;
 
 public class SavingsCalculator {
-    public float[] credits;
-    public float[] debts;
 
-    public SavingsCalculator(float[] credits, float[] debts) {
-        this.credits = credits;
-        this.debts = debts;
+    private static float[] credits;
+    private static float[] debits;
+
+    public SavingsCalculator(float[] credits1, float[] debits1) {
+        this.credits = credits1;
+        this.debits = debits1;
     }
 
     private static float sumOfCredits(float[] credits) {
+
         float sum = 0.0f;
+
         for (int i = 0; i < credits.length; i++) {
             sum += credits[i];
         }
+
         return sum;
     }
 
     private static float sumOfDebits(float[] debits) {
+
         float sum = 0.0f;
+
         for (int i = 0; i < debits.length; i++) {
             sum += debits[i];
         }
@@ -29,14 +35,13 @@ public class SavingsCalculator {
     }
 
     private static int remainingDaysInMonth(LocalDate date) {
-        YearMonth yearMonth = null;
-        assert yearMonth != null;
+        YearMonth yearMonth = YearMonth.of(date.getYear(), date.getMonth());
         int totalDaysInMonth = yearMonth.lengthOfMonth();
         int remainingDays = totalDaysInMonth - date.getDayOfMonth();
         return remainingDays;
     }
 
-    private static float calculate(float[] credits, float[] debts) {
+    public static float calculate(float[] credits, float[] debts) {
         return sumOfDebits(debts) - sumOfCredits(credits);
     }
 
@@ -61,3 +66,4 @@ public class SavingsCalculator {
     }
 }
 
+//Net Savings = -5.0, remaining days in month = 11
